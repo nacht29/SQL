4c9264b6a160ff39666ac4cd57b678d465be652c
nacht29 <nacht29.study@gmail.com>
2024-12-26 20:26:12 +0800

    refactor(dannys_diner.sql): styled main query

--- 8-weeks-SQL-challenge/danny_diner/dannys_diner.sql
+++ 8-weeks-SQL-challenge/danny_diner/dannys_diner.sql
@@ -1,19 +1,12 @@
--- Q1 - Q2
--- basic analysis on visit frequency and spending pattern
-SELECT
-	m.customer_id,
-    SUM(me.price) AS amount_spent,
-    COUNT(DISTINCT s.order_date) AS number_of_visits,
-    ROUND(SUM(me.price) / COUNT(DISTINCT s.order_date),  2) AS expense_per_visit
+-- Question 1 
+SELECT 
+  sales.customer_id, 
+  SUM(menu.price) AS total_sales
 FROM
-	members AS m
-LEFT JOIN sales AS s
-	ON s.customer_id = m.customer_id
-LEFT JOIN menu AS me
-	ON me.product_id = s.product_id
+	sales
+INNER JOIN menu
+  ON sales.product_id = menu.product_id
 GROUP BY
-	m.customer_id;
-
--- Q3 - Q5
--- menu item and preference analysis
--- will have to learn CTE for this
+	sales.customer_id
+ORDER BY
+	sales.customer_id ASC; 
